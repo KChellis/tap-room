67363420cb8496e659d680b6d84ea523f9aa2d69
Kristen Chellis <peanutster@gmail.com>
2018-05-02 16:08:03 -0700

    color code by beer lightness

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,13 +8,13 @@ import { Keg } from './models/keg.model';
 })
 export class AppComponent {
   masterKegList: Keg[] = [
-    new Keg('Superfuzz', 'Elysian Brewing', 'Blood Orange Pale Ale', 6.4, 45, [5.00, 9.00, 18.00]),
-    new Keg('Citrus Mistress', 'Hop Valley Brewing', 'IPA', 6.3, 80, [5.00, 9.00, 18.00]),
-    new Keg('Black Butte Porter', 'Dechutes Brewing', 'Porter', 5.2, 30, [5.00, 9.00, 18.00]),
-    new Keg('Cucumber Crush', '10 Barrel Brewing', 'Sour', 5, 4, [4.50, 8.50, 17.00]),
-    new Keg('Pabst Blue Ribbon', 'Pabst Brewing', 'Piss', 4.7, 10, [3.00, 6.00, 12.00]),
-    new Keg('Maid Marion', '2 Towns Ciderhouse', 'Marionberry Cider', 6, 0, [5.00, 9.00, 18.00]),
-    new Keg('Willamette Valley Pinot Noir', 'Boedecker Cellars', 'Pinot Noir', 14, 0, [6.00, 11.00, 22.00])
+    new Keg('Superfuzz', 'Elysian Brewing', 'Blood Orange Pale Ale', 6.4, 45, [5.00, 9.00, 18.00], 'light'),
+    new Keg('Citrus Mistress', 'Hop Valley Brewing', 'IPA', 6.3, 80, [5.00, 9.00, 18.00], 'light'),
+    new Keg('Black Butte Porter', 'Dechutes Brewing', 'Porter', 5.2, 30, [5.00, 9.00, 18.00], 'dark'),
+    new Keg('Cucumber Crush', '10 Barrel Brewing', 'Sour', 5, 4, [4.50, 8.50, 17.00], 'light'),
+    new Keg('Pabst Blue Ribbon', 'Pabst Brewing', 'Piss', 4.7, 10, [3.00, 6.00, 12.00], 'light'),
+    new Keg('Maid Marion', '2 Towns Ciderhouse', 'Marionberry Cider', 6, 0, [5.00, 9.00, 18.00], 'cider'),
+    new Keg('Willamette Valley Pinot Noir', 'Boedecker Cellars', 'Pinot Noir', 14, 0, [6.00, 11.00, 22.00], 'wine')
   ]
 
     selectedKeg: Keg = null;
@@ -40,7 +40,7 @@ export class AppComponent {
       this.selectedKeg = null;
     }
     addKeg() {
-      let keg: Keg = new Keg('New keg', '', '', null, null, []);
+      let keg: Keg = new Keg('New keg', '', '', null, null, [],'');
       this.selectedKeg = keg;
       this.masterKegList.push(keg);
     }
--- src/app/models/keg.model.ts
+++ src/app/models/keg.model.ts
@@ -1,4 +1,4 @@
 export class Keg {
   pints: number = 124;
-  constructor(public name: string, public brand: string,public type: string, public abv: number, public ibu: number, public price: number[]) {}
+  constructor(public name: string, public brand: string,public type: string, public abv: number, public ibu: number, public price: number[], public color: string) {}
 }
